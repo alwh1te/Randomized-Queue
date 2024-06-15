8668388b537e1f119fb09a9a574490e681fc2c25
alw <alexander.s.filatov@gmail.com>
2024-06-15 19:35:50 +0300

    all tests passed fith std::conditional_t

--- RandomizedQueue.hpp
+++ RandomizedQueue.hpp
@@ -11,60 +11,67 @@ class RandomizedQueue {
     using value_type = T;
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
-    using reference = value_type &;
-    using const_reference = const value_type &;
-    using pointer = value_type *;
-    using const_pointer = const value_type *;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
 
-    class iterator {
+    template <bool IsConst>
+    class iterator_impl {
+        using queue_type = std::conditional_t<IsConst, const RandomizedQueue, RandomizedQueue>;
+        using ref_type = std::conditional_t<IsConst, const_reference, reference>;
+        using ptr_type = std::conditional_t<IsConst, const_pointer, pointer>;
     public:
         using iterator_category = std::forward_iterator_tag;
         using value_type = T;
         using difference_type = std::ptrdiff_t;
-        using pointer = T*;
-        using reference = T&;
+        using pointer = ptr_type;
+        using reference = ref_type;
 
-        iterator() : m_queue(nullptr), m_pos(0) {}
-        iterator(const RandomizedQueue* queue, size_type pos) : m_queue(queue), m_pos(pos) {
+        iterator_impl() : m_queue(nullptr), m_pos(0) {}
+        iterator_impl(queue_type* queue, size_type pos) : m_queue(queue), m_pos(pos) {
             if (queue) {
                 m_permuted_indices = queue->get_permuted_indexes();
             }
         }
 
         reference operator*() const {
-            return const_cast<reference>(m_queue->m_data[m_permuted_indices[m_pos]]);
+            return m_queue->m_data[m_permuted_indices[m_pos]];
         }
 
         pointer operator->() const {
-            return const_cast<pointer>(&m_queue->m_data[m_permuted_indices[m_pos]]);
+            return &m_queue->m_data[m_permuted_indices[m_pos]];
         }
 
-        iterator &operator++() {
+        iterator_impl& operator++() {
             ++m_pos;
             return *this;
         }
 
-        iterator operator++(int) {
-            iterator temp = *this;
+        iterator_impl operator++(int) {
+            iterator_impl temp = *this;
             ++(*this);
             return temp;
         }
 
-        bool operator==(const iterator &other) const {
+        bool operator==(const iterator_impl& other) const {
             return m_queue == other.m_queue && m_pos == other.m_pos;
         }
 
-        bool operator!=(const iterator &other) const {
+        bool operator!=(const iterator_impl& other) const {
             return !(*this == other);
         }
 
     private:
-        const RandomizedQueue* m_queue;
+        queue_type* m_queue;
         size_type m_pos;
         std::vector<size_type> m_permuted_indices;
     };
 
 public:
+    using iterator = iterator_impl<false>;
+    using const_iterator = iterator_impl<true>;
+
     RandomizedQueue() : m_gen(std::random_device{}()) {}
 
     bool empty() const noexcept {
@@ -111,21 +118,22 @@ public:
         return m_data[dist(m_gen)];
     }
 
-    RandomizedQueue &operator+=(const_reference item) {
+    RandomizedQueue& operator+=(const_reference item) {
         enqueue(item);
         return *this;
     }
 
-    RandomizedQueue &operator+=(value_type&& item) {
+    RandomizedQueue& operator+=(value_type&& item) {
         enqueue(std::move(item));
         return *this;
     }
 
-    iterator begin() const noexcept { return iterator(this, 0); }
-    iterator end() const noexcept { return iterator(this, m_data.size()); }
-    iterator cbegin() const noexcept { return iterator(const_cast<RandomizedQueue*>(this), 0); }
-    iterator cend() const noexcept { return iterator(const_cast<RandomizedQueue*>(this), m_data.size()); }
-
+    iterator begin() noexcept { return iterator(this, 0); }
+    iterator end() noexcept { return iterator(this, m_data.size()); }
+    const_iterator begin() const noexcept { return const_iterator(this, 0); }
+    const_iterator end() const noexcept { return const_iterator(this, m_data.size()); }
+    const_iterator cbegin() const noexcept { return const_iterator(this, 0); }
+    const_iterator cend() const noexcept { return const_iterator(this, m_data.size()); }
 
 private:
     std::vector<size_type> get_permuted_indexes() const noexcept {
